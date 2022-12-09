4b5c8eb53856a1bbb15bbb8757276dee923d3cdb
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2022-12-09 09:54:41 +0400

    Fixed overflow and added custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ function wordCount() {
 </script>
 
 <template>
-  <div class="relative flex h-screen w-screen justify-center bg-stone-900">
+  <div class="relative flex h-full w-full justify-center bg-stone-900">
     <div class="flex h-full w-full max-w-2xl flex-col gap-4 py-4">
       <input
         type="text"
@@ -36,13 +36,15 @@ function wordCount() {
         class="bg-transparent text-4xl text-stone-200 outline-none placeholder:italic placeholder:text-stone-500"
         v-model="data.title"
       />
-      <div
-        ref="contentDiv"
-        class="relative bg-transparent text-xl text-stone-200 outline-none"
-        placeholder="content"
-        contenteditable
-        @input="(e: any) => (data.content = e.target.innerText)"
-      ></div>
+      <div class="grow overflow-y-auto">
+        <div
+          ref="contentDiv"
+          class="relative bg-transparent text-xl text-stone-200 outline-none"
+          placeholder="content"
+          contenteditable
+          @input="(e: any) => (data.content = e.target.innerText)"
+        ></div>
+      </div>
     </div>
     <div class="text-stone-400">
       <div class="absolute top-4 right-4 flex flex-col items-end">
